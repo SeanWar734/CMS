489f2f31d3b2a13275b3348a179aa8306a3cd246
Sean <s.warchuck16@gmail.com>
2020-10-14 23:00:09 -0400

    got the project running and returns json data for two contacts i created, i dont know where the database is located though

--- WebApplication1/Data/Contact.cs
+++ WebApplication1/Data/Contact.cs
@@ -12,5 +12,10 @@ namespace WebApplication1.Data
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
+
+        public override String ToString()
+        {
+            return "Name: " + this.FirstName + " " + this.LastName + " | Phone Number: " + this.PhoneNumber + " | Birth Date: " + this.BirthDate;
+        }
     }
 }
